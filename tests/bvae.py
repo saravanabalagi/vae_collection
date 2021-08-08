571232fa197fe5a1076ca4454c138dfffb5b3bcd
Saravanabalagi Ramachandran <saravanabalagi@hotmail.com>
2021-08-08 22:33:51 +0100

    Add pytest Fix contiguous bug

--- tests/bvae.py
+++ tests/bvae.py
@@ -8,7 +8,7 @@ class TestVAE(unittest.TestCase):
 
     def setUp(self) -> None:
         # self.model2 = VAE(3, 10)
-        self.model = BetaVAE(3, 10, loss_type='H').cuda()
+        self.model = BetaVAE(3, 10, loss_type='H')
 
     def test_summary(self):
         print(summary(self.model, (3, 64, 64), device='cpu'))
@@ -21,7 +21,7 @@ class TestVAE(unittest.TestCase):
         # print("Model2 Output size:", self.model2(x)[0].size())
 
     def test_loss(self):
-        x = torch.randn(16, 3, 64, 64).cuda()
+        x = torch.randn(16, 3, 64, 64)
 
         result = self.model(x)
         loss = self.model.loss_function(*result, M_N = 0.005)
